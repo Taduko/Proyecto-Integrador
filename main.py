bcb07ebff26cadfbbd1e232867550052ba9e093d
Taduko <leopoldoj.calix@gmail.com>
2023-10-18 02:02:52 -0600

    Segunda parte del proyecto integrador, liberia readchar, bucle sale cuando se apreta la tecla UP

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 #Inicio del juego
+from readchar import readkey, key
 
 nombre = input("Ingrese su nombre: ")
 print("Bienvenido al juego", nombre)
+
+#Bucle infinito hasta apretar la tecla UP
+
+while True:
+    tecla = readkey()
+    if tecla == key.UP:
+        print("Has presionado la tecla UP")
+        break
+    else:
+        print("Tecla incorrecta")
